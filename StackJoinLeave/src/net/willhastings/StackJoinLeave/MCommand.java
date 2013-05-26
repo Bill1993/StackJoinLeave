4fefe2788c8f79f50e68b38c3bb62ea8d7066031
William B Hastings <willbhastings@gmail.com>
2013-05-26 12:35:35 -0700

    /messages toggle, and changed how it announces who joines and leaves.

---
+++ StackJoinLeave/src/net/willhastings/StackJoinLeave/MCommand.java
@@ -0,0 +1,44 @@
+package net.willhastings.StackJoinLeave;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+public class MCommand implements CommandExecutor 
+{	
+	public static ArrayList<String> ignored = new ArrayList<String>();
+	
+	private void invalidCommand(CommandSender sender)
+	{
+		sender.sendMessage(ChatColor.RED + "Invalid or Missing Sub-Command!");
+		sender.sendMessage("/messages toggle");
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]) 
+	{
+		if(sender instanceof Player)
+		{
+			if(args.length > 0 && args[0].equalsIgnoreCase("Toggle"))
+			{
+				if(ignored.contains(sender.getName()))
+				{
+					ignored.remove(sender.getName());
+					sender.sendMessage("You will now see the stacked join leave messages!");
+				}
+				else
+				{
+					ignored.add(sender.getName());
+					sender.sendMessage("You will no longer see the stacked join leave messages!");
+				}
+			}
+			else this.invalidCommand(sender);
+			return true;
+		}
+		return false;
+	}
+}
--- StackJoinLeave/src/net/willhastings/StackJoinLeave/Main.java
+++ StackJoinLeave/src/net/willhastings/StackJoinLeave/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -23,6 +24,7 @@ public class Main extends JavaPlugin implements Listener
 	{
 		this.getServer().getPluginManager().registerEvents(this, this);
 		task = new JoinLeaveMessage().runTaskTimer(this, 20*60*3, 20*60*3);
+		this.getCommand("messages").setExecutor(new MCommand());
 	}
 	
 	public void onDisable()
@@ -65,43 +67,34 @@ class JoinLeaveMessage extends BukkitRunnable
 	
 	public void run() 
 	{	
-		if(Main.pJoin.size() > 0)
+		if(Main.pJoin.size() > 0 || Main.pQuit.size() > 0)
 		{
 			temp = "";
-			if(Main.pJoin.size() < 2)
+			if(Main.pJoin.size() > 0)
 			{
-				Bukkit.broadcastMessage(ChatColor.GRAY + "In the past 3 min " + Main.pJoin.get(0) + " has joined the server!");
+				
+				if(Main.pJoin.size() < 2) temp = ChatColor.GRAY + "In the past 3 min " + Main.pJoin.get(0) + " has joined";
+				else if(Main.pJoin.size() < 3) temp = ChatColor.GRAY + "In the past 3 min " + Main.pJoin.get(0) + ", and " + Main.pJoin.get(1) + " have joined";
+				else temp = ChatColor.GRAY + "In the past 3 min " + Main.pJoin.get(0) + ", " + Main.pJoin.get(1) + ", and " + (Main.pJoin.size() - 1) + " others have joined";
+				Main.pJoin.clear();
 			}
-			else if(Main.pJoin.size() < 3)
+			else temp = ChatColor.GRAY + "In the past 3 min no one has joined";
+			
+			if(Main.pQuit.size() > 0)
 			{
-				Bukkit.broadcastMessage(ChatColor.GRAY + "In the past 3 min " + Main.pJoin.get(0) + ", and " + Main.pJoin.get(1) + " have joined the server!");
+				if(Main.pQuit.size() < 2) temp += ", and " + Main.pQuit.get(0) + " has left the server!";
+				else if(Main.pQuit.size() < 3) temp += ", and " + Main.pQuit.get(0) + ", and " + Main.pQuit.get(1) + " have left the server!";
+				else temp += ", and " + Main.pQuit.get(0) + ", " + Main.pQuit.get(1) + ", and " + (Main.pQuit.size() - 1) + " others have left the server!";
+				Main.pQuit.clear();
 			}
-			else
+			else temp += ", and no one has left the server!";
+			
+			for(Player p: Bukkit.getOnlinePlayers())
 			{
-				Bukkit.broadcastMessage(ChatColor.GRAY + "In the past 3 min " + Main.pJoin.get(0) + ", " + Main.pJoin.get(1) + ", and " + (Main.pJoin.size() - 1) 
-						+ " others have joined the sever!");
+				if(!MCommand.ignored.contains(p.getName())) p.sendMessage(temp);
+				
 			}
-			Main.pJoin.clear();
+			
 		}
-		
-		if(Main.pQuit.size() > 0)
-		{
-			temp = "";
-			if(Main.pQuit.size() < 2)
-			{
-				Bukkit.broadcastMessage(ChatColor.GRAY + "In the past 3 min " + Main.pQuit.get(0) + " has left the server!");
-			}
-			else if(Main.pQuit.size() < 3)
-			{
-				Bukkit.broadcastMessage(ChatColor.GRAY + "In the past 3 min " + Main.pQuit.get(0) + ", and " + Main.pQuit.get(1) + " have left the server!");
-			}
-			else
-			{
-				Bukkit.broadcastMessage(ChatColor.GRAY + "In the past 3 min " + Main.pQuit.get(0) + ", " + Main.pQuit.get(1) + ", and " + (Main.pQuit.size() - 1) 
-						+ " others have left the sever!");
-			}
-			Main.pQuit.clear();
-		}
-	}
-	
+	}	
 }
