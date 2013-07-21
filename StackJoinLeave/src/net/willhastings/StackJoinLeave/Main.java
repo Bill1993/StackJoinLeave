945e55f600c61030524fe948b8d1a39d1b502872
William B Hastings <willbhastings@gmail.com>
2013-07-21 09:38:50 -0700

    her der

--- StackJoinLeave/src/net/willhastings/StackJoinLeave/Main.java
+++ StackJoinLeave/src/net/willhastings/StackJoinLeave/Main.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -45,12 +46,24 @@ public class Main extends JavaPlugin implements Listener
 		event.setJoinMessage(null);
 	}
 	
+	public void onPlayerKick(PlayerKickEvent event)
+	{
+		Boolean bool = false;
+		String quit = event.getPlayer().getDisplayName();
+		for(String i: pQuit)
+		{
+			if(i.equalsIgnoreCase(quit)) bool = true;
+		}
+		if(!bool) pQuit.add(quit);
+		event.setLeaveMessage(null);
+	}
+	
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event)
 	{
 		Boolean bool = false;
 		String quit = event.getPlayer().getDisplayName();
-		for(String i: pJoin)
+		for(String i: pQuit)
 		{
 			if(i.equalsIgnoreCase(quit)) bool = true;
 		}
